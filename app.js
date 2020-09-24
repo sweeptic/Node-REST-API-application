cd5a2ccc914cdd50152389cf278d3e4ebf30e6fb
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-24 13:04:32 +0200

    8. REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ const app = express();
 
 app.use(bodyParser.json());   //application/json
 
+
+//set header any response -because we built rest api
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*'); //* all domain
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+   //client can send extra authorization  data in the header
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Content-Type, Authorization');
+   next();
+});
+
 //i can parse incoming request bodies
 //app use -  any method
 app.use('/feed', feedRoutes);
