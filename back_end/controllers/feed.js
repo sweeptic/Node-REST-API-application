eb6351fac8ce7cff7eaf6a9e5adb5364930bb421
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-25 11:48:15 +0200

    9. Storing Posts in the Database

--- back_end/controllers/feed.js
+++ back_end/controllers/feed.js
@@ -1,5 +1,8 @@
 const { validationResult } = require('express-validator');
 
+
+const Post = require('../models/post')
+
 //title, author, date, image, content
 
 exports.getPosts = (req, res, next) => {
@@ -27,6 +30,7 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
 
+
    //server side validation
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
@@ -38,23 +42,37 @@ exports.createPost = (req, res, next) => {
          })
    }
 
-
+   //get data from body
    const title = req.body.title;
    const content = req.body.content;
-   //create post in bd
 
-   //this is just only confirmation that it was stored successfully
-   res.status(201).json({
-      message: 'Post created successfully',
-      post: {
-         _id: new Date().toISOString(),
-         title: title,
-         content: content,
-         creator: {
-            name: 'SurferBoy',
-            createdAt: new Date()
-         }
-
-      }
+   //create a new post
+   const post = new Post({
+      title: title,
+      content: content,
+      creator: { name: 'SurferBoy' },
+      imageUrl: 'images/IMG_1257.jpg'
    })
+
+   //save post
+   post.save()
+
+      //get success result
+      .then((result) => {
+         console.log(result);
+
+
+         //this is just only confirmation that it was stored successfully
+         res.status(201).json({
+            message: 'Post created successfully',
+            post: result
+         })
+
+
+      })
+      //get unsuccessfull result if there is any
+      .catch(err => console.log(err))
+
+
+
 }
