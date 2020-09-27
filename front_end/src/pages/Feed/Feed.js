096cf42c7c7d71467fccb0c5d3c89aced86c59cc
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-27 14:11:25 +0200

    uploading image

--- front_end/src/pages/Feed/Feed.js
+++ front_end/src/pages/Feed/Feed.js
@@ -105,32 +105,20 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
+    const formData = new FormData();
+    formData.append('title', postData.title);
+    formData.append('content', postData.content);
+    formData.append('image', postData.image);
     let url = 'http://localhost:8080/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
       url = 'URL';
     }
 
-
-
-
     fetch(url, {
       method: method,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: postData.title,
-        content: postData.content
-      })
+      body: formData
     })
-
-
-
-
-
-
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
@@ -138,7 +126,7 @@ class Feed extends Component {
         return res.json();
       })
       .then(resData => {
-        console.log(resData)
+        console.log(resData);
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
