5a5ac2fb684d34b7ea466cfc05c49dfdf34eab46
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-28 12:31:29 +0200

    26. Adding Authorization Checks

--- back_end/controllers/feed.js
+++ back_end/controllers/feed.js
@@ -157,6 +157,14 @@ exports.updatePost = (req, res, next) => {
             error.statusCode = 404;
             throw error;
          }
+
+         if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+         }
+
+
          if (imageUrl !== post.imageUrl) {
             clearImage(post.imageUrl);
          }
@@ -187,6 +195,12 @@ exports.deletePost = (req, res, next) => {
          if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
+            throw error;
+         }
+
+         if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
             throw error;
          }
 
