241d3f11c7cce19bb5295783bfbd2e0e5c0d1579
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-28 12:39:09 +0200

    27. Clearing Post-User Relations

--- back_end/controllers/feed.js
+++ back_end/controllers/feed.js
@@ -208,10 +208,16 @@ exports.deletePost = (req, res, next) => {
          return Post.findByIdAndRemove(postId)
       })
       .then(result => {
-         console.log(result);
+         return User.findById(req.userId)
+
+      })
+      .then(user => {
+         user.posts.pull(postId)
+         return user.save()
+      })
+      .then(result => {
          res.status(200).json({ message: 'Post deleted!' });
       })
-
       .catch(err => {
          if (!err.statusCode) {
             err.statusCode = 500;
